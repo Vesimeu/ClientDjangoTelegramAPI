5bf15d00430bbb46c75ed24107368cc03036ee2f
Vesimeu <pyoter.poliakov@yandex.ru>
2024-05-11 04:00:55 +0500

    Переписал telegram_bot на aiogram. Теперь два бота ассинхронны!

--- myshop/mainshop/management/commands/telegram_bot.py
+++ myshop/mainshop/management/commands/telegram_bot.py
@@ -1,77 +1,94 @@
-# myshop/management/commands/telegram_bot.py
-from django.core.management.base import BaseCommand
-from telegram import Bot, Update
-from telegram.ext import CommandHandler, MessageHandler, Filters, Updater, CallbackContext
-from telegram.utils.request import Request
-from django.utils import timezone
-from myshop import settings  # Импорт настроек
-from mainshop import models
-Order = models.Order
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.utils import executor
+from channels.db import database_sync_to_async
+from django.core.management import BaseCommand
+
+from myshop import settings
+from mainshop.models import Order
+
 class Command(BaseCommand):
     help = 'Starts the telegram bot for orders.'
 
     def handle(self, *args, **options):
-        # Создаем объект запроса с использованием прокси
-        request = Request(connect_timeout=15)
+        bot_token = settings.TOKEN_ADMIN
+        telegram_bot = TelegramBot(bot_token)
+        telegram_bot.start_bot()
 
-        # Создаем объект бота с использованием запроса и токена
-        bot = Bot(request=request, token=settings.TOKEN_ADMIN)
-        print(bot)
+class TelegramBot:
+    def __init__(self, bot_token):
+        self.bot = Bot(token=bot_token)
+        self.dp = Dispatcher(self.bot)
+        self.dp.middleware.setup(LoggingMiddleware())
 
-        # Обработчик команды /start
-        def start(update: Update, context: CallbackContext):
-            update.message.reply_text("Добро пожаловать! Этот бот поможет вам сделать заказ через наш сайт.")
-        def help(update: Update, context: CallbackContext):
-            update.message.reply_text("/accept {number} - принять заказ \n /decline {number} - отказать \n /order_list - список заказов.")
+        # Регистрация обработчиков команд
+        self.dp.register_message_handler(self.start, commands=['start'])
+        self.dp.register_message_handler(self.help, commands=['help'])
+        self.dp.register_message_handler(self.accept_order, commands=['accept'])
+        self.dp.register_message_handler(self.decline_order, commands=['decline'])
+        self.dp.register_message_handler(self.order_list, commands=['order_list'])
 
-        # Обработчик команды /accept
-        def accept_order(update: Update, context: CallbackContext):
-            if context.args:
-                order_id = context.args[0]  # Предположим, что аргументом передается ID заказа
-                order = Order.objects.get(id=order_id)
-                # Здесь вы можете обновить статус заказа в базе данных, например:
-                order.accepted = True
-                order.save()
-                update.message.reply_text(f"Заказ #{order_id} принят.")
-                print("Заказ действиетльно принят!" + str(order.accepted))
-            else:
-                update.message.reply_text("Вы не указали ID заказа.")
+    async def start(self, message: types.Message):
+        await message.answer("Добро пожаловать! Этот бот поможет вам сделать заказ через наш сайт.")
 
-        # Обработчик команды /decline
-        def decline_order(update: Update, context: CallbackContext):
-            order_id = context.args[0]  # Предположим, что аргументом передается ID заказа
-            order = Order.objects.get(id=order_id)
-            # Здесь вы можете обновить статус заказа в базе данных, например:
-            order.accepted = False
-            order.save()
-            update.message.reply_text(f"Заказ #{order_id} отклонен.")
+    async def help(self, message: types.Message):
+        help_message = "/accept {number} - принять заказ\n/decline {number} - отказать\n/order_list - список заказов."
+        await message.answer(help_message)
 
-        def order_list(update: Update, context: CallbackContext):
-            orders = Order.objects.all()  # Получаем все заказы из базы данных
-            if orders:
-                message = "Список заказов:\n"
-                for order in orders:
-                    message += f"Заказ #{order.id} от @{order.nickname}:\n{order.description}\nЦена: {order.price}\n\n" #Добавить статус заказа
-            else:
-                message = "Список заказов пуст."
-            update.message.reply_text(message)
+    async def accept_order(self, message: types.Message):
+        args = message.get_args().split()
+        if args:
+            order_id = args[0]
+            try:
+                order = await self.get_order(order_id)
+                await self.process_order(order)
+                await message.answer(f"Заказ #{order_id} принят.")
+            except Order.DoesNotExist:
+                await message.answer("Заказ с указанным номером не найден.")
+        else:
+            await message.answer("Вы не указали номер заказа.")
 
-        # Создаем обработчики команд
-        start_handler = CommandHandler('start', start)
-        help = CommandHandler('help', help)
-        accept_handler = CommandHandler('accept', accept_order)
-        decline_handler = CommandHandler('decline', decline_order)
-        order_list_handler = CommandHandler('order_list', order_list)
+    @database_sync_to_async
+    def get_order(self, order_id):
+        return Order.objects.get(id=order_id)
 
-        # Создаем и регистрируем диспетчер для обработки команд
-        updater = Updater(bot=bot, use_context=True)
-        dp = updater.dispatcher
-        dp.add_handler(start_handler)
-        dp.add_handler(help)
-        dp.add_handler(accept_handler)
-        dp.add_handler(decline_handler)
-        dp.add_handler(order_list_handler)
+    @database_sync_to_async
+    def process_order(self, order):
+        order.accepted = True
+        order.save()
 
-        # Запускаем бота
-        updater.start_polling()
-        updater.idle()
+    async def decline_order(self, message: types.Message):
+        args = message.get_args().split()
+        if args:
+            order_id = args[0]
+            try:
+                order = await self.get_order(order_id)
+                await self.decline_order_async(order)
+                await message.answer(f"Заказ #{order_id} отклонен.")
+            except Order.DoesNotExist:
+                await message.answer("Заказ с указанным номером не найден.")
+        else:
+            await message.answer("Вы не указали номер заказа.")
+
+    @database_sync_to_async
+    def decline_order_async(self, order):
+        order.accepted = False
+        order.save()
+
+    async def order_list(self, message: types.Message):
+        orders = await self.get_all_orders()
+        if orders:
+            order_list_message = "Список заказов:\n"
+            for order in orders:
+                order_list_message += f"Заказ #{order.id} от @{order.nickname}:\n{order.description}\nЦена: {order.price}\n\n"
+            await message.answer(order_list_message)
+        else:
+            await message.answer("Список заказов пуст.")
+
+    @database_sync_to_async
+    def get_all_orders(self):
+        return Order.objects.all()
+
+    def start_bot(self):
+        executor.start_polling(self.dp, skip_updates=True)
